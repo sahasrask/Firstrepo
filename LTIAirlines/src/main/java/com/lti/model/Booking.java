49cf3bfca94cb14e1238e14154173546d7d8c4b6
sahas <sahas@192.168.0.105>
2021-09-15 12:14:01 +0530

    Booking.java is added

---
+++ LTIAirlines/src/main/java/com/lti/model/Booking.java
@@ -0,0 +1,22 @@
+package com.lti.model;
+
+public class Booking {
+
+	int bookingId;
+	Flight flight;
+	public int getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+	
+	
+
+}
