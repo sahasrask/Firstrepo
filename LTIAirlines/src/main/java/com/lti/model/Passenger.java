065e5bfaa6c8cdb5a07526ac6caa2ffe56777f8e
sahas <sahas@192.168.0.105>
2021-09-15 13:18:16 +0530

    passenger is added to br1

---
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -0,0 +1,17 @@
+package com.lti.model;
+
+public class Passenger {
+	
+	String passengerName;
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	
+	
+
+}
