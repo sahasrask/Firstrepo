6588c05bafb55038a037091884d57fc8033ac003
sahas <sahas@192.168.0.105>
2021-09-15 13:22:22 +0530

    modified passenger thru br1

--- LTIAirlines/src/main/java/com/lti/model/Passenger.java
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -3,6 +3,35 @@ package com.lti.model;
 public class Passenger {
 	
 	String passengerName;
+	int passengerId;
+	String address;
+	int age;
+	
+	
+
+	public int getPassengerId() {
+		return passengerId;
+	}
+
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	public String getPassengerName() {
 		return passengerName;
